b01b1200ab6f40f01f026712080a33326e677473
DiegoxK <diego.stv@outlook.es>
2025-07-31 17:43:17 -0500

    layout x3

--- src/app/(editor)/page.tsx
+++ src/app/(editor)/page.tsx
@@ -3,43 +3,42 @@ import Image from "next/image";
 
 export default function Editor() {
   return (
-    <main className="flex grow flex-col">
+    <main className="flex grow flex-col justify-between">
       <div className="border-b p-4">
         <nav>Navigation</nav>
       </div>
-
-      <div className="relative h-full w-full">
-        <div className="overflow-hidden">
+      <div className="flex grow items-center justify-center overflow-hidden">
+        <div className="relative w-full">
           <AspectRatio ratio={16 / 9}>
-            <Image
+            <img
               src="/defaults/background.jpg"
-              width={1920}
-              height={1080}
               alt="background"
-              className="w-full object-cover"
+              className="w-full object-fill"
             />
           </AspectRatio>
-        </div>
-        <div
-          className="absolute inset-0"
-          style={{
-            perspective: "80vh",
-            perspectiveOrigin: "50% 100%",
-          }}
-        >
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src="/defaults/highway.png"
-            alt="highway"
-            className="absolute bottom-0 left-1/2 -translate-x-1/2 mask-t-from-80%"
+          <div
+            className="absolute inset-0"
             style={{
-              width: "80%",
-              height: "600%",
-              transform: "rotateX(60deg) translateZ(-200px)",
-              transformOrigin: "50% 100%",
+              perspective: "80vh",
+              perspectiveOrigin: "50% 100%",
             }}
-          />
+          >
+            <img
+              src="/defaults/highway.png"
+              alt="highway"
+              className="absolute bottom-0 left-1/2 -translate-x-1/2 mask-t-from-80%"
+              style={{
+                width: "80%",
+                height: "600%",
+                transform: "rotateX(60deg) translateZ(-200px)",
+                transformOrigin: "50% 100%",
+              }}
+            />
+          </div>
         </div>
+      </div>
+      <div className="border-t p-4">
+        <nav>Navigation</nav>
       </div>
     </main>
   );
