73598b9c330c42dd04e7b250ef5b89111b9165cd
DiegoxK <diego.stv@outlook.es>
2025-07-31 18:35:56 -0500

    layoooout

--- src/app/(editor)/_components/layout/sidebar.tsx
+++ src/app/(editor)/_components/layout/sidebar.tsx
@@ -7,10 +7,12 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function Sidebar() {
   return (
-    <aside className="flex w-80 flex-col border-r">
+    <aside className="flex w-94 flex-col border-r">
       <div className="border-b p-4">
         <div className="flex gap-4">
           <h2>
@@ -34,20 +36,33 @@ export default function Sidebar() {
         >
           <Accordion type="multiple">
             <AccordionItem value="item-1">
-              <AccordionTrigger>Is it accessible?</AccordionTrigger>
+              <AccordionTrigger>Notes</AccordionTrigger>
               <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
+                <div className="flex items-center gap-3">
+                  <div className="size-9 flex-shrink-0 rounded-md bg-green-200" />
+                  <Input />
+                  <Button size="icon">
+                    <ChevronDown />
+                  </Button>
+                </div>
+                <div className="flex items-center gap-3">
+                  <div className="size-9 flex-shrink-0 rounded-md bg-red-200" />
+                  <Input />
+                  <Button size="icon">
+                    <ChevronDown />
+                  </Button>
+                </div>
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-2">
-              <AccordionTrigger>Is it styled?</AccordionTrigger>
+              <AccordionTrigger>Glow</AccordionTrigger>
               <AccordionContent>
                 Yes. It comes with default styles that match the other
                 aesthetic.
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-3">
-              <AccordionTrigger>Does it scroll correctly now?</AccordionTrigger>
+              <AccordionTrigger>Fires</AccordionTrigger>
               <AccordionContent>
                 Yes. By making the TabsContent a flex item that grows to fill
                 all available space, the overflow property now works correctly
@@ -66,9 +81,7 @@ export default function Sidebar() {
               </AccordionContent>
             </AccordionItem>
             <AccordionItem value="item-4">
-              <AccordionTrigger>
-                More Content to cause overflow
-              </AccordionTrigger>
+              <AccordionTrigger>Other</AccordionTrigger>
               <AccordionContent>
                 More Content to cause overflow
               </AccordionContent>
