bda3a0437747f23ff434b60425474b986700d1da
DiegoxK <diego.stv@outlook.es>
2025-08-01 18:25:08 -0500

    working strikers

--- src/app/(editor)/page.tsx
+++ src/app/(editor)/page.tsx
@@ -22,10 +22,12 @@ export default function Editor() {
             alt=""
             className="absolute inset-0 opacity-100"
           />
-          {/* <Highway />
-          <Sidebars /> */}
-          <Strings />
+          {/* <Highway /> */}
+          {/* <Strings /> */}
+          {/* <BeatLines /> */}
+          {/* <Sidebars /> */}
           {/* <Notes /> */}
+          {/* <Strikers /> */}
         </div>
       </div>
       <div className="border-t p-4">
@@ -47,7 +49,7 @@ const Highway = () => {
       <img
         src="/defaults/highway.png"
         alt=""
-        className="absolute bottom-0 left-1/2 -translate-x-1/2 mask-t-from-80%"
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 mask-t-from-90%"
         style={{
           width: "66%",
           height: "600%",
@@ -107,40 +109,212 @@ const Sidebars = () => {
 
 const Strings = () => {
   return (
-    <div
-      className="absolute inset-0"
-      style={{
-        perspective: "110vh",
-        perspectiveOrigin: "50% 100%",
-      }}
-    >
-      <img
-        src="/sprites/layout/Guitarstring_wor_remake2.png"
-        alt=""
-        className="absolute right-[38%] bottom-53 -translate-x-1/2 mask-t-from-10%"
+    <>
+      <div
+        className="absolute inset-0"
         style={{
-          width: "3%",
-          height: "280%",
-          transform: "rotateX(60deg) translateZ(-200px) ",
-
-          transformOrigin: "50% 100%",
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
         }}
-      />
-    </div>
+      >
+        <img
+          src="/sprites/layout/Guitarstring_wor_remake2.png"
+          alt=""
+          className="absolute right-[29%] bottom-53 -translate-x-1/2 mask-t-from-10%"
+          style={{
+            width: "3%",
+            height: "250%",
+            transform: "rotateX(60deg) translateZ(-200px) ",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-0"
+        style={{
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
+        }}
+      >
+        <img
+          src="/sprites/layout/Guitarstring_wor_remake2.png"
+          alt=""
+          className="absolute right-[38%] bottom-53 -translate-x-1/2 mask-t-from-10%"
+          style={{
+            width: "3%",
+            height: "250%",
+            transform: "rotateX(60deg) translateZ(-200px) ",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-0"
+        style={{
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
+        }}
+      >
+        <img
+          src="/sprites/layout/Guitarstring_wor_remake2.png"
+          alt=""
+          className="absolute right-[47%] bottom-53 -translate-x-1/2 mask-t-from-10%"
+          style={{
+            width: "3%",
+            height: "250%",
+            transform: "rotateX(60deg) translateZ(-200px) ",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-0"
+        style={{
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
+        }}
+      >
+        <img
+          src="/sprites/layout/Guitarstring_wor_remake2.png"
+          alt=""
+          className="absolute bottom-53 left-[32%] -translate-x-1/2 mask-t-from-10%"
+          style={{
+            width: "3%",
+            height: "250%",
+            transform: "rotateX(60deg) translateZ(-200px) ",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-0"
+        style={{
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
+        }}
+      >
+        <img
+          src="/sprites/layout/Guitarstring_wor_remake2.png"
+          alt=""
+          className="absolute bottom-53 left-[41%] -translate-x-1/2 mask-t-from-10%"
+          style={{
+            width: "3%",
+            height: "250%",
+            transform: "rotateX(60deg) translateZ(-200px) ",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+    </>
   );
 };
 
-const BeatLine = () => {
+const BeatLines = () => {
   return (
-    <img
-      src="/sprites/layout/beatline.png"
-      alt=""
-      className="absolute right-[38%] bottom-53 -translate-x-1/2"
-      style={{
-        width: "3%",
-        height: "280%",
-        transformOrigin: "50% 100%",
-      }}
-    />
+    <>
+      <img
+        src="/sprites/layout/beatline.png"
+        alt=""
+        className="absolute bottom-38 left-[50%] -translate-x-1/2"
+        style={{
+          width: "27%",
+        }}
+      />
+      <img
+        src="/sprites/layout/beatline.png"
+        alt=""
+        className="absolute bottom-64 left-[50%] -translate-x-1/2"
+        style={{
+          width: "18%",
+        }}
+      />
+    </>
+  );
+};
+
+const Strikers = () => {
+  const Striker = ({
+    position,
+    headColor,
+    ringColor,
+  }: {
+    position: number;
+    headColor: string;
+    ringColor: string;
+  }) => {
+    return (
+      <div className="relative h-19 w-19">
+        <Image
+          src={`/sprites/guitar/striker_GuitarUnderlay${position}.png`}
+          alt=""
+          fill={true}
+          className="object-contain"
+        />
+        <Image
+          src={`/sprites/guitar/striker_GuitarHead${position}.png`}
+          alt=""
+          fill={true}
+          className="object-contain"
+        />
+
+        <Image
+          src={`/sprites/guitar/striker_HeadColor${position}.png`}
+          alt=""
+          fill={true}
+          className="object-contain"
+        />
+
+        <div
+          className="absolute inset-0 mix-blend-multiply"
+          style={{
+            backgroundColor: headColor,
+            maskImage: `url(/sprites/guitar/striker_HeadColor${position}.png)`,
+            WebkitMaskImage: `url(/sprites/guitar/striker_HeadColor${position}.png)`,
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+
+        {/* <Image
+          src="/sprites/guitar/striker_HeadGlow1.png"
+          alt=""
+          fill={true}
+          className="object-contain"
+        /> */}
+        <Image
+          src={`/sprites/guitar/striker_Ring${position}.png`}
+          alt=""
+          fill={true}
+          className="object-contain"
+        />
+        <div
+          className="absolute inset-0 mix-blend-multiply"
+          style={{
+            backgroundColor: ringColor,
+            maskImage: `url(/sprites/guitar/striker_Ring${position}.png)`,
+            WebkitMaskImage: `url(/sprites/guitar/striker_Ring${position}.png)`,
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+      </div>
+    );
+  };
+
+  return (
+    <div className="absolute bottom-[5%] left-[50%] flex -translate-x-1/2">
+      <Striker position={1} headColor="#00FF00" ringColor="#00FF00" />
+      <Striker position={2} headColor="#FF0000" ringColor="#FF0000" />
+      <Striker position={3} headColor="#FFFF00" ringColor="#FFFF00" />
+      <Striker position={4} headColor="#0089FF" ringColor="#0089FF" />
+      <Striker position={5} headColor="#FFB300" ringColor="#FFB300" />
+    </div>
   );
 };
