598d7a73f86ad6c501ed57202b68d60dd6370b3f
DiegoxK <diego.stv@outlook.es>
2025-08-01 17:31:37 -0500

    lane basic layout

--- src/app/(editor)/page.tsx
+++ src/app/(editor)/page.tsx
@@ -1,5 +1,6 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from "next/image";
+import Notes from "./_components/editor/notes";
 
 export default function Editor() {
   return (
@@ -16,30 +17,130 @@ export default function Editor() {
               className="w-full object-fill"
             />
           </AspectRatio>
-          <div
-            className="absolute inset-0"
-            style={{
-              perspective: "80vh",
-              perspectiveOrigin: "50% 100%",
-            }}
-          >
-            <img
-              src="/defaults/highway.png"
-              alt="highway"
-              className="absolute bottom-0 left-1/2 -translate-x-1/2 mask-t-from-80%"
-              style={{
-                width: "80%",
-                height: "600%",
-                transform: "rotateX(60deg) translateZ(-200px)",
-                transformOrigin: "50% 100%",
-              }}
-            />
-          </div>
+          <img
+            src="/example.png"
+            alt=""
+            className="absolute inset-0 opacity-100"
+          />
+          {/* <Highway />
+          <Sidebars /> */}
+          <Strings />
+          {/* <Notes /> */}
         </div>
       </div>
       <div className="border-t p-4">
-        <nav>Navigation</nav>
+        <nav>Heads | Notes</nav>
       </div>
     </main>
   );
 }
+
+const Highway = () => {
+  return (
+    <div
+      className="absolute inset-0"
+      style={{
+        perspective: "110vh",
+        perspectiveOrigin: "50% 100%",
+      }}
+    >
+      <img
+        src="/defaults/highway.png"
+        alt=""
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 mask-t-from-80%"
+        style={{
+          width: "66%",
+          height: "600%",
+          transform: "rotateX(60deg) translateZ(-200px)",
+          transformOrigin: "50% 100%",
+        }}
+      />
+    </div>
+  );
+};
+const Sidebars = () => {
+  return (
+    <>
+      <div
+        className="absolute inset-0"
+        style={{
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
+        }}
+      >
+        <img
+          src="/sprites/layout/sidebar.png"
+          alt=""
+          className="absolute bottom-0 left-[14.3%] -translate-x-1/2 mask-t-from-80%"
+          style={{
+            width: "7%",
+            height: "600%",
+            transform: "rotateX(60deg) translateZ(-200px)",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+      <div
+        className="absolute inset-0"
+        style={{
+          perspective: "110vh",
+          perspectiveOrigin: "50% 100%",
+        }}
+      >
+        <img
+          src="/sprites/layout/sidebar.png"
+          alt=""
+          className="absolute right-[7.3%] bottom-0 -translate-x-1/2 mask-t-from-80%"
+          style={{
+            width: "7%",
+            height: "600%",
+            transform: "rotateX(60deg) translateZ(-200px) scaleX(-1)",
+
+            transformOrigin: "50% 100%",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+
+const Strings = () => {
+  return (
+    <div
+      className="absolute inset-0"
+      style={{
+        perspective: "110vh",
+        perspectiveOrigin: "50% 100%",
+      }}
+    >
+      <img
+        src="/sprites/layout/Guitarstring_wor_remake2.png"
+        alt=""
+        className="absolute right-[38%] bottom-53 -translate-x-1/2 mask-t-from-10%"
+        style={{
+          width: "3%",
+          height: "280%",
+          transform: "rotateX(60deg) translateZ(-200px) ",
+
+          transformOrigin: "50% 100%",
+        }}
+      />
+    </div>
+  );
+};
+
+const BeatLine = () => {
+  return (
+    <img
+      src="/sprites/layout/beatline.png"
+      alt=""
+      className="absolute right-[38%] bottom-53 -translate-x-1/2"
+      style={{
+        width: "3%",
+        height: "280%",
+        transformOrigin: "50% 100%",
+      }}
+    />
+  );
+};
