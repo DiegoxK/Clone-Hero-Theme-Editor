aca871538d853f74547320d8bef66f15749538eb
DiegoxK <diego.stv@outlook.es>
2025-08-04 21:15:13 -0500

    finished powermeter

--- src/app/(editor)/page.tsx
+++ src/app/(editor)/page.tsx
@@ -22,11 +22,11 @@ export default function Editor() {
             alt=""
             className="absolute inset-0 w-full opacity-100"
           />
-          <Highway />
-          <Strings />
-          <BeatLines />
-          <Sidebars />
-          <Strikers />
+          {/* <Highway /> */}
+          {/* <Strings /> */}
+          {/* <BeatLines /> */}
+          {/* <Sidebars /> */}
+          {/* <Strikers /> */}
           {/* <Notes /> */}
           <PowerMetter />
         </div>
@@ -268,14 +268,25 @@ const Strikers = () => {
 
 const PowerMetter = () => {
   return (
-    <img
-      src="/sprites/other/spr_starpower_meter_strip5_0.png"
-      alt="Star Power Meter"
-      className="absolute right-[31.6%] bottom-[12%]"
-      style={{
-        width: "1.9%",
-        transform: "translateX(-50%) rotate(-26deg) scaleX(-1)",
-      }}
-    />
+    <>
+      <img
+        src="/sprites/other/spr_starpower_meter_strip5_0.png"
+        alt="Star Power Meter"
+        className="absolute right-[31.6%] bottom-[12%]"
+        style={{
+          width: "1.9%",
+          transform: "translateX(-50%) rotate(-26deg) scaleX(-1)",
+        }}
+      />
+      <img
+        src="/sprites/other/spr_starpower_meter_strip5_4.png"
+        alt="Star Power Meter"
+        className="absolute right-[32.7%] bottom-[24%]"
+        style={{
+          width: "1.2%",
+          transform: "translateX(-50%) rotate(-26deg) scaleX(-1)",
+        }}
+      />
+    </>
   );
 };
