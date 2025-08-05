a0d1403f9bbd00186ff3efb019046966038fee86
DiegoxK <diego.stv@outlook.es>
2025-08-05 17:52:21 -0500

    added import theme button

--- src/app/(editor)/page.tsx
+++ src/app/(editor)/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { AspectRatio } from "@/components/ui/aspect-ratio";
-import Image from "next/image";
 import Notes from "./_components/editor/notes";
-import { useState } from "react";
-import { initialThemeData } from "@/lib/default-theme";
+import { useRef, useState, type ChangeEvent } from "react";
+import { initialThemeData, type Theme } from "@/lib/default-theme";
 import Highway from "./_components/editor/highway";
 import GuitarStrings from "./_components/editor/guitar-strings";
 import BeatLines from "./_components/editor/beatlines";
@@ -15,14 +13,54 @@ import Strikers from "./_components/editor/strikers";
 import PowerMetter from "./_components/editor/power-metter";
 import BgImage from "./_components/editor/bg-image";
 import OpenNote from "./_components/editor/open-note";
+import { parseINIString } from "@/lib/ini-utils";
+import { Button } from "@/components/ui/button";
 
 export default function Editor() {
   const [theme, setTheme] = useState(initialThemeData);
 
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const text = e.target?.result;
+      if (typeof text === "string") {
+        try {
+          const parsedData = parseINIString(text);
+
+          setTheme(parsedData as Theme);
+        } catch (error) {
+          console.error("Failed to parse INI file:", error);
+          alert("Error: Could not parse the INI file.");
+        }
+      }
+    };
+
+    reader.readAsText(file);
+  };
+
+  const handleUploadClick = () => {
+    fileInputRef.current?.click();
+  };
   return (
     <main className="flex grow flex-col justify-between">
-      <div className="border-b p-4">
-        <nav>Navigation</nav>
+      <div className="flex border-b p-3">
+        <Button size="sm" onClick={handleUploadClick}>
+          Load colors.ini
+        </Button>
+
+        <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          accept=".ini"
+          className="hidden"
+        />
       </div>
       <div className="flex grow items-center justify-center overflow-hidden">
         <div className="[container-type:inline-size] relative w-full">
