c118c44dfcee9f4b558487fd98a36d24c70463dd
DiegoxK <diego.stv@outlook.es>
2025-08-05 12:25:00 -0500

    implemented pressed strikers

--- src/app/(editor)/page.tsx
+++ src/app/(editor)/page.tsx
@@ -28,7 +28,7 @@ export default function Editor() {
           <MultiplierCircle />
           <DotRow />
           <Sidebars />
-          <Strikers state="regular" />
+          <Strikers />
           {/* <Notes /> */}
           <PowerMetter />
         </div>
@@ -187,52 +187,109 @@ const BeatLines = () => {
 };
 
 const strikerData = [
-  { key: 1, position: 1, headColor: "#00FF00", ringColor: "#00FF00" },
-  { key: 2, position: 2, headColor: "#FF0000", ringColor: "#FF0000" },
-  { key: 3, position: 3, headColor: "#FFFF00", ringColor: "#FFFF00" },
-  { key: 4, position: 4, headColor: "#0089FF", ringColor: "#0089FF" },
-  { key: 5, position: 5, headColor: "#FFB300", ringColor: "#FFB300" },
+  {
+    key: 1,
+    position: 1,
+    headColor: "#00FF00",
+    headLight: "#00FF00",
+    ringColor: "#00FF00",
+  },
+  {
+    key: 2,
+    position: 2,
+    headColor: "#FF0000",
+    headLight: "#FF0000",
+    ringColor: "#FF0000",
+  },
+  {
+    key: 3,
+    position: 3,
+    headColor: "#FFFF00",
+    headLight: "#FFFF00",
+    ringColor: "#FFFF00",
+  },
+  {
+    key: 4,
+    position: 4,
+    headColor: "#0089FF",
+    headLight: "#0089FF",
+    ringColor: "#0089FF",
+  },
+  {
+    key: 5,
+    position: 5,
+    headColor: "#FFB300",
+    headLight: "#FFB300",
+    ringColor: "#FFB300",
+  },
 ];
 
 const Striker = ({
+  pressed,
   position,
   headColor,
   ringColor,
+  headLight,
 }: {
+  pressed: boolean;
   position: number;
   headColor: string;
   ringColor: string;
+  headLight: string;
 }) => {
   return (
     <div className="relative aspect-square w-[7.5cqw]">
-      <img
-        src={`/sprites/guitar/striker_GuitarUnderlay${position}.png`}
-        alt=""
-        className="absolute inset-0 h-full w-full object-contain"
-      />
-      <img
-        src={`/sprites/guitar/striker_GuitarHead${position}.png`}
-        alt=""
-        className="absolute inset-0 h-full w-full object-contain"
-      />
-      <img
-        src={`/sprites/guitar/striker_HeadColor${position}.png`}
-        alt=""
-        className="absolute inset-0 h-full w-full object-contain"
-      />
+      {pressed ? (
+        <>
+          <div
+            className="absolute inset-0"
+            style={{
+              backgroundColor: headLight,
+              maskImage: `url(/sprites/guitar/striker_DownGlow_${position}.png)`,
+              WebkitMaskImage: `url(/sprites/guitar/striker_DownGlow_${position}.png)`,
+              maskRepeat: "no-repeat",
+              maskSize: "contain",
+              maskPosition: "center",
+            }}
+          />
 
-      <div
-        className="absolute inset-0 mix-blend-multiply"
-        style={{
-          backgroundColor: headColor,
-          maskImage: `url(/sprites/guitar/striker_HeadColor${position}.png)`,
-          WebkitMaskImage: `url(/sprites/guitar/striker_HeadColor${position}.png)`,
-          maskRepeat: "no-repeat",
-          maskSize: "contain",
-          maskPosition: "center",
-        }}
-      />
+          <img
+            src={`/sprites/guitar/striker_down_${position}.png`}
+            alt=""
+            className="absolute inset-0 h-full w-full object-contain mix-blend-screen"
+          />
+        </>
+      ) : (
+        <>
+          <img
+            src={`/sprites/guitar/striker_GuitarUnderlay${position}.png`}
+            alt=""
+            className="absolute inset-0 h-full w-full object-contain"
+          />
+          <img
+            src={`/sprites/guitar/striker_GuitarHead${position}.png`}
+            alt=""
+            className="absolute inset-0 h-full w-full object-contain"
+          />
+          <img
+            src={`/sprites/guitar/striker_HeadColor${position}.png`}
+            alt=""
+            className="absolute inset-0 h-full w-full object-contain"
+          />
 
+          <div
+            className="absolute inset-0 mix-blend-multiply"
+            style={{
+              backgroundColor: headColor,
+              maskImage: `url(/sprites/guitar/striker_HeadColor${position}.png)`,
+              WebkitMaskImage: `url(/sprites/guitar/striker_HeadColor${position}.png)`,
+              maskRepeat: "no-repeat",
+              maskSize: "contain",
+              maskPosition: "center",
+            }}
+          />
+        </>
+      )}
       <img
         src={`/sprites/guitar/striker_Ring${position}.png`}
         alt=""
@@ -259,9 +316,11 @@ const Strikers = () => {
       {strikerData.map((striker) => (
         <Striker
           key={striker.key}
+          pressed={false}
           headColor={striker.headColor}
           position={striker.position}
           ringColor={striker.ringColor}
+          headLight={striker.headLight}
         />
       ))}
     </div>
