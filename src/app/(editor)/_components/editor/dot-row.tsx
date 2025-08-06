fcf76e773b1215afd4f50dbc4afb00995710175c
DiegoxK <diego.stv@outlook.es>
2025-08-06 08:13:02 -0500

    added color mask to dot row

--- src/app/(editor)/_components/editor/dot-row.tsx
+++ src/app/(editor)/_components/editor/dot-row.tsx
@@ -9,14 +9,25 @@ const DotRow = () => {
           width: "4%",
         }}
       />
-      <img
-        src="/sprites/other/dotrow_1.png"
-        alt="Star Power Meter"
-        className="absolute right-[36.9%] bottom-[36.8%] rotate-2"
-        style={{
-          width: "3%",
-        }}
-      />
+      <div className="absolute right-[36.9%] bottom-[36.7%] aspect-[57/84] w-[3cqw] rotate-1">
+        <img
+          src="/sprites/other/dotrow_1.png"
+          alt=""
+          className="absolute inset-0 h-full w-full object-contain"
+        />
+
+        <div
+          className="absolute inset-0 mix-blend-multiply"
+          style={{
+            backgroundColor: "#00D8D8",
+            maskImage: "url(/sprites/other/dotrow_1.png)",
+            WebkitMaskImage: "url(/sprites/other/dotrow_1.png)",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+      </div>
     </>
   );
 };
