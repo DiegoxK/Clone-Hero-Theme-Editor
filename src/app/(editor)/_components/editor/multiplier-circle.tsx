b0cc1a54fe599d83ba6424adab660a15a16fb37a
DiegoxK <diego.stv@outlook.es>
2025-08-06 09:03:28 -0500

    added colored number to multiplier number

--- src/app/(editor)/_components/editor/multiplier-circle.tsx
+++ src/app/(editor)/_components/editor/multiplier-circle.tsx
@@ -1,9 +1,15 @@
-const MultiplierCircle = () => {
+import type { Theme } from "@/lib/default-theme";
+
+type MultiplierCircleProps = {
+  otherColors: Theme["other"];
+};
+
+const MultiplierCircle = ({ otherColors }: MultiplierCircleProps) => {
   return (
     <>
       <img
         src="/sprites/other/spr_multiplier_circle_strip3_1.png"
-        alt="Star Power Meter"
+        alt=""
         className="absolute right-[28%] bottom-[25.55%]"
         style={{
           width: "13%",
@@ -11,15 +17,51 @@ const MultiplierCircle = () => {
       />
       <img
         src="/sprites/other/spr_multiplier_circle_strip3_2.png"
-        alt="Star Power Meter"
+        alt=""
         className="absolute right-[28%] bottom-[25.55%]"
         style={{
           width: "13%",
         }}
       />
+      <div className="absolute right-[33.1%] bottom-[34.8%] aspect-[48/56] w-[2.2cqw]">
+        <img
+          src="/sprites/other/highway_Multiplier_NumberGlow4.png"
+          alt=""
+          className="absolute inset-0 h-full w-full object-contain"
+        />
+        <div
+          className="absolute inset-0 mix-blend-normal"
+          style={{
+            backgroundColor: otherColors.combo_four_glow,
+            maskImage: "url(/sprites/other/highway_Multiplier_NumberGlow4.png)",
+            WebkitMaskImage:
+              "url(/sprites/other/highway_Multiplier_NumberGlow4.png)",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+        <img
+          src="/sprites/other/highway_Multiplier_Number4.png"
+          alt=""
+          className="absolute inset-0 h-full w-full object-contain opacity-45"
+        />
+        <div
+          className="absolute inset-0 mix-blend-overlay"
+          style={{
+            backgroundColor: otherColors.combo_four,
+            maskImage: "url(/sprites/other/highway_Multiplier_Number4.png)",
+            WebkitMaskImage:
+              "url(/sprites/other/highway_Multiplier_Number4.png)",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+      </div>
       <img
         src="/sprites/other/spr_multiplier_circle_strip3_0.png"
-        alt="Star Power Meter"
+        alt=""
         className="absolute right-[28%] bottom-[25.55%]"
         style={{
           width: "13%",
