672c691c04fe37fc04b95d4bb6d34938f3871e4a
DiegoxK <diego.stv@outlook.es>
2025-08-11 18:40:54 -0500

    fixed assets and blend methods

--- src/app/(editor)/_components/editor/power-metter.tsx
+++ src/app/(editor)/_components/editor/power-metter.tsx
@@ -3,26 +3,47 @@ const PowerMetter = () => {
     <>
       <img
         src="/sprites/other/spr_starpower_meter_strip5_0.png"
-        alt="Star Power Meter"
+        alt=""
         className="absolute right-[31.6%] bottom-[12%]"
         style={{
           width: "1.9%",
           transform: "translateX(-50%) rotate(-26deg) scaleX(-1)",
         }}
       />
-      <div className="absolute right-[31.6%] bottom-[14.2%] aspect-[31/124] w-[1.63cqw] -scale-x-[1] rotate-[-27deg]">
+
+      <div className="absolute right-[31.46%] bottom-[14.5%] aspect-[36/119] w-[1.8cqw] -scale-x-[1] rotate-[-26deg]">
         <img
-          src="/sprites/other/spr_starpower_meter_strip5_1.png"
-          alt="Star Power Meter"
-          className="absolute inset-0 h-full w-full object-contain opacity-50"
+          src="/sprites/other/spr_starpower_meter_strip5_fragment.png"
+          alt=""
+          className="absolute inset-0 h-full w-full object-contain"
+        />
+        <div
+          className="absolute inset-0 mix-blend-plus-lighter"
+          style={{
+            backgroundColor: "#004848",
+            maskImage:
+              "url(/sprites/other/spr_starpower_meter_strip5_fragment.png)",
+            WebkitMaskImage:
+              "url(/sprites/other/spr_starpower_meter_strip5_fragment.png)",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+      </div>
+      <div className="absolute right-[29%] bottom-[12.2%] aspect-square w-[4.2cqw] -scale-x-[1] rotate-[-18deg]">
+        <img
+          src="/sprites/other/spr_rockmeter_needle_strip3_0.png"
+          alt=""
+          className="absolute inset-0 h-full w-full object-contain opacity-60"
         />
         <div
           className="absolute inset-0 mix-blend-overlay"
           style={{
-            backgroundColor: "#004848",
-            maskImage: "url(/sprites/other/spr_starpower_meter_strip5_1.png)",
+            backgroundColor: "#7FFFFF",
+            maskImage: "url(/sprites/other/spr_rockmeter_needle_strip3_0.png)",
             WebkitMaskImage:
-              "url(/sprites/other/spr_starpower_meter_strip5_1.png)",
+              "url(/sprites/other/spr_rockmeter_needle_strip3_0.png)",
             maskRepeat: "no-repeat",
             maskSize: "contain",
             maskPosition: "center",
@@ -31,13 +52,32 @@ const PowerMetter = () => {
       </div>
       <img
         src="/sprites/other/spr_starpower_meter_strip5_4.png"
-        alt="Star Power Meter"
+        alt=""
         className="absolute right-[32.7%] bottom-[24%]"
         style={{
           width: "1.2%",
           transform: "translateX(-50%) rotate(-26deg) scaleX(-1)",
         }}
       />
+      <div className="absolute right-[32.1%] bottom-[21.3%] aspect-square w-[3.1cqw] -scale-x-[1] rotate-[-30deg]">
+        <img
+          src="/sprites/other/spr_rockmeter_needle_strip3_0.png"
+          alt=""
+          className="absolute inset-0 h-full w-full object-contain opacity-60"
+        />
+        <div
+          className="absolute inset-0 mix-blend-overlay"
+          style={{
+            backgroundColor: "#7FFFFF",
+            maskImage: "url(/sprites/other/spr_rockmeter_needle_strip3_0.png)",
+            WebkitMaskImage:
+              "url(/sprites/other/spr_rockmeter_needle_strip3_0.png)",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+      </div>
     </>
   );
 };
