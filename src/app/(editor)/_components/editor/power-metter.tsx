bc1139fecc3bd1a88145f4e83dfc9794f926b5dc
DiegoxK <diego.stv@outlook.es>
2025-08-11 17:23:39 -0500

    added starpower meter with hardcoded color

--- src/app/(editor)/_components/editor/power-metter.tsx
+++ src/app/(editor)/_components/editor/power-metter.tsx
@@ -10,6 +10,25 @@ const PowerMetter = () => {
           transform: "translateX(-50%) rotate(-26deg) scaleX(-1)",
         }}
       />
+      <div className="absolute right-[31.5%] bottom-[14%] aspect-[31/124] w-[1.63cqw] -scale-x-[1] rotate-[-27deg]">
+        <img
+          src="/sprites/other/spr_starpower_meter_strip5_1.png"
+          alt="Star Power Meter"
+          className="absolute inset-0 h-full w-full object-contain"
+        />
+        <div
+          className="absolute inset-0 mix-blend-overlay"
+          style={{
+            backgroundColor: "#FFFF00",
+            maskImage: "url(/sprites/other/spr_starpower_meter_strip5_1.png)",
+            WebkitMaskImage:
+              "url(/sprites/other/spr_starpower_meter_strip5_1.png)",
+            maskRepeat: "no-repeat",
+            maskSize: "contain",
+            maskPosition: "center",
+          }}
+        />
+      </div>
       <img
         src="/sprites/other/spr_starpower_meter_strip5_4.png"
         alt="Star Power Meter"
