c57f3e7077015d4e60e3d0c6376d7ef33e071e72
DiegoxK <diego.stv@outlook.es>
2025-08-20 11:06:07 -0500

    refactor default theme to generate along with a set of presets

---
+++ scripts/key-mappings.mjs
@@ -0,0 +1,28 @@
+// This file defines the mapping from old, outdated color keys
+// to the new keys used in the current 1.0.0.4080 version of the game.
+
+export const KEY_MAPPINGS = {
+  drums: {
+    striker_base_green: "drums_striker_base_green",
+    striker_base_red: "drums_striker_base_red",
+    striker_base_yellow: "drums_striker_base_yellow",
+    striker_base_blue: "drums_striker_base_blue",
+
+    striker_cover_green: "drums_striker_cover_green",
+    striker_cover_red: "drums_striker_cover_red",
+    striker_cover_yellow: "drums_striker_cover_yellow",
+    striker_cover_blue: "drums_striker_cover_blue",
+
+    striker_head_cover_green: "drums_striker_head_cover_green",
+    striker_head_cover_red: "drums_striker_head_cover_red",
+    striker_head_cover_yellow: "drums_striker_head_cover_yellow",
+    striker_head_cover_blue: "drums_striker_head_cover_blue",
+
+    striker_head_light_green: "drums_striker_head_light_green",
+    striker_head_light_red: "drums_striker_head_light_red",
+    striker_head_light_yellow: "drums_striker_head_light_yellow",
+    striker_head_light_blue: "drums_striker_head_light_blue",
+    striker_head_light_kick: "drums_striker_head_light_kick",
+    note_overlay_kick_sp_active: "note_overlay_kick_sp_phrase",
+  },
+};
