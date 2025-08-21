afc592643708075820328830a35214d38b04a4f6
DiegoxK <diego.stv@outlook.es>
2025-08-21 10:05:16 -0500

    added history to the main theme store to save the last 100 changes for undo and redo functionality

--- src/hooks/stores/use-theme-store.ts
+++ src/hooks/stores/use-theme-store.ts
@@ -1,6 +1,10 @@
 import { create } from "zustand";
+import { temporal, type TemporalState } from "zundo";
+import { useStoreWithEqualityFn } from "zustand/traditional";
 import { persist, createJSONStorage } from "zustand/middleware";
 import { type Theme, initialThemeData } from "@/lib/presets/index";
+import { shallow } from "zustand/shallow";
+import debounce from "lodash.debounce";
 
 type ThemeState = {
   theme: Theme;
@@ -11,31 +15,54 @@ type ThemeState = {
 };
 
 export const useThemeStore = create<ThemeState>()(
-  persist(
-    (set) => ({
-      theme: initialThemeData,
-      themeName: "Default Theme",
-      setTheme: (newTheme, newName) =>
-        set({ theme: newTheme, themeName: newName }),
-      updateColor: (section, key, value) =>
-        set((state) => ({
-          theme: {
-            ...state.theme,
+  temporal(
+    persist(
+      (set) => ({
+        theme: initialThemeData,
+        themeName: "Default Theme",
+        setTheme: (newTheme, newName) =>
+          set({ theme: newTheme, themeName: newName }),
+        updateColor: (section, key, value) =>
+          set((state) => ({
+            theme: {
+              ...state.theme,
 
-            [section]: {
-              ...state.theme[section],
+              [section]: {
+                ...state.theme[section],
 
-              [key]: value.toUpperCase(),
+                [key]: value.toUpperCase(),
+              },
             },
-          },
-          themeName: "Custom Theme",
-        })),
+            themeName: "Custom Theme",
+          })),
 
-      reset: () => set({ theme: initialThemeData, themeName: "Default Theme" }),
-    }),
+        reset: () =>
+          set({ theme: initialThemeData, themeName: "Default Theme" }),
+      }),
+      {
+        name: "clone-hero-theme-storage",
+        storage: createJSONStorage(() => localStorage),
+      },
+    ),
     {
-      name: "clone-hero-theme-storage",
-      storage: createJSONStorage(() => localStorage),
+      handleSet: (handleSet) =>
+        debounce<typeof handleSet>(
+          (state) => {
+            handleSet(state);
+          },
+          1000,
+          {
+            leading: true,
+            trailing: false,
+          },
+        ),
+      limit: 100,
     },
   ),
 );
+
+export const useTemporalThemeStore = <T>(
+  selector: (state: TemporalState<Partial<ThemeState>>) => T,
+) => {
+  return useStoreWithEqualityFn(useThemeStore.temporal, selector, shallow);
+};
