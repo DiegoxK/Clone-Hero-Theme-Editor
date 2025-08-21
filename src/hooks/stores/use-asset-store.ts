0c5116a81242158955cc8681d9042e13a071878d
DiegoxK <diego.stv@outlook.es>
2025-08-21 12:26:21 -0500

    remove asset persistance

--- src/hooks/stores/use-asset-store.ts
+++ src/hooks/stores/use-asset-store.ts
@@ -27,32 +27,24 @@ const initialAssetState = {
 const STRIKER_CYCLE_ORDER: StrikerState[] = ["default", "pressed", "open"];
 const NOTE_CYCLE_ORDER: NoteState[] = ["default", "sp"];
 
-export const useAssetStore = create<AssetStoreState>()(
-  persist(
-    (set) => ({
-      ...initialAssetState,
+export const useAssetStore = create<AssetStoreState>((set) => ({
+  ...initialAssetState,
 
-      cycleStrikerState: () =>
-        set((state) => {
-          const currentIndex = STRIKER_CYCLE_ORDER.indexOf(state.strikerState);
-          const nextIndex = (currentIndex + 1) % STRIKER_CYCLE_ORDER.length;
-          return { strikerState: STRIKER_CYCLE_ORDER[nextIndex] };
-        }),
-
-      cycleNoteState: () =>
-        set((state) => {
-          const currentIndex = NOTE_CYCLE_ORDER.indexOf(state.noteState);
-          const nextIndex = (currentIndex + 1) % NOTE_CYCLE_ORDER.length;
-          return { noteState: NOTE_CYCLE_ORDER[nextIndex] };
-        }),
-      setBackgroundUrl: (url) => set({ backgroundUrl: url }),
-      setHighwayUrl: (url) => set({ highwayUrl: url }),
-
-      reset: () => set(initialAssetState),
+  cycleStrikerState: () =>
+    set((state) => {
+      const currentIndex = STRIKER_CYCLE_ORDER.indexOf(state.strikerState);
+      const nextIndex = (currentIndex + 1) % STRIKER_CYCLE_ORDER.length;
+      return { strikerState: STRIKER_CYCLE_ORDER[nextIndex] };
     }),
-    {
-      name: "clone-hero-asset-storage",
-      storage: createJSONStorage(() => localStorage),
-    },
-  ),
-);
+
+  cycleNoteState: () =>
+    set((state) => {
+      const currentIndex = NOTE_CYCLE_ORDER.indexOf(state.noteState);
+      const nextIndex = (currentIndex + 1) % NOTE_CYCLE_ORDER.length;
+      return { noteState: NOTE_CYCLE_ORDER[nextIndex] };
+    }),
+  setBackgroundUrl: (url) => set({ backgroundUrl: url }),
+  setHighwayUrl: (url) => set({ highwayUrl: url }),
+
+  reset: () => set(initialAssetState),
+}));
