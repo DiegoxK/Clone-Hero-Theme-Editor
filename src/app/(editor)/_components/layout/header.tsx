b3f70ff7da82092d52b2a44733eec15bde97bb98
DiegoxK <diego.stv@outlook.es>
2025-08-22 10:18:15 -0500

    added link button to the main repo

--- src/app/(editor)/_components/layout/header.tsx
+++ src/app/(editor)/_components/layout/header.tsx
@@ -1,9 +1,21 @@
+import { Button } from "@/components/ui/button";
+import { Github } from "lucide-react";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="border-b p-4">
+    <header className="flex items-center justify-between border-b p-4">
       <h1 className="text-primary text-xl font-bold">
         Clone Hero Theme Editor
       </h1>
+      <Link
+        target="_blank"
+        href="https://github.com/DiegoxK/clone-hero-theme-editor"
+      >
+        <Button className="rounded-full" variant="ghost" size="icon">
+          <Github className="size-4.5" />
+        </Button>
+      </Link>
     </header>
   );
 }
